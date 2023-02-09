7101a61125e4ca20f5f1bea877ff4fb73d295ead
saugatshakya <saugatoff@gmail.com>
2023-02-09 11:09:31 +0545

    web and mobile first page

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:testapp/presentation/home.dart';
-import 'package:testapp/static/colors.dart';
+import 'package:testapp/temp_home_page.dart';
+import 'package:testapp/temp_home_phone.dart';
+import 'package:universal_html/html.dart' as html;
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final String phoneType = "phone";
+
+  final String desktopType = "desktop";
+
+  String getSmartPhoneOrTablet() {
+    final userAgent = html.window.navigator.userAgent.toString().toLowerCase();
+    // smartphone
+    if (userAgent.contains("iphone") || userAgent.contains("android")) {
+      return phoneType;
+    }
+    // tablet
+    else {
+      return desktopType;
+    }
+  }
+
+  @override
+  void initState() {
+    print(getSmartPhoneOrTablet());
+    super.initState();
+  }
 
   // This widget is the root of your application.
   @override
@@ -21,7 +49,9 @@ class MyApp extends StatelessWidget {
           primarySwatch: Colors.green,
           fontFamily: GoogleFonts.ptSans().fontFamily,
           useMaterial3: true),
-      home: Home(),
+      home: getSmartPhoneOrTablet() == phoneType
+          ? const TempHomePhone()
+          : const TempHomePage(),
     );
   }
 }
